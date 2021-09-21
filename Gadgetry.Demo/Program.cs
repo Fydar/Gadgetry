1d07922a2a65540277e5f37cbd8519f5e9cfdb69
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-09-21 02:57:33 +0100

    ✨ Extend sample to include a counter demo

---
+++ Gadgetry.Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Gadgetry.Demo
+{
+	internal class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			var task = CounterSample.CreateCounterSample();
+
+			var runner = task.CreateRunner();
+
+			await runner.RunAsync();
+		}
+	}
+}
