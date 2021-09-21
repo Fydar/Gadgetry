394ed078916a85cc10d03359d35b44ded758a352
fydar <fydar.dev@gmail.com>
2021-09-21 02:55:30 +0100

    ✨ Added a way of adding multiple sub-gadgets to run in parallel

---
+++ Gadgetry/Workers/GadgetWorker.cs
@@ -0,0 +1,25 @@
+﻿namespace Gadgetry.Workers
+{
+	public class GadgetWorker
+	{
+		public Gadget WorkerGadget { get; }
+		public GadgetWorkerOptions Options { get; }
+
+		internal GadgetWorker(Gadget workerGadget)
+		{
+			WorkerGadget = workerGadget;
+			Options = new GadgetWorkerOptions();
+		}
+
+		internal GadgetWorker(Gadget workerGadget, GadgetWorkerOptions options)
+		{
+			WorkerGadget = workerGadget;
+			Options = options;
+		}
+
+		public override string ToString()
+		{
+			return $"{Options.Workers}x {WorkerGadget}";
+		}
+	}
+}
---
+++ Gadgetry/Workers/IGadgetBuilderExtensions.cs
@@ -0,0 +1,67 @@
+﻿namespace Gadgetry.Workers
+{
+	public static class IGadgetBuilderExtensions
+	{
+		public static IGadgetBuilder UseWorker(
+			this IGadgetBuilder gadgetBuilder,
+			Gadget workerGadget)
+		{
+			gadgetBuilder.Configure(configure =>
+			{
+				var feature = configure.Features.GetOrCreateFeature<GadgetWorkersFeature>();
+
+				feature.workers.Add(new GadgetWorker(workerGadget));
+			});
+
+			return gadgetBuilder;
+		}
+
+		public static IGadgetBuilder UseWorker(
+			this IGadgetBuilder gadgetBuilder,
+			Gadget workerGadget,
+			GadgetWorkerOptions workerGadgetOptions)
+		{
+			gadgetBuilder.Configure(configure =>
+			{
+				var feature = configure.Features.GetOrCreateFeature<GadgetWorkersFeature>();
+
+				feature.workers.Add(new GadgetWorker(workerGadget, workerGadgetOptions));
+			});
+
+			return gadgetBuilder;
+		}
+
+		public static IGadgetBuilder UseWorker(
+			this IGadgetBuilder gadgetBuilder,
+			IGadgetBuilder workerGadget)
+		{
+			gadgetBuilder.Configure(configure =>
+			{
+				var feature = configure.Features.GetOrCreateFeature<GadgetWorkersFeature>();
+
+				string stepIdentifier = $"{configure.Identifier}-worker{feature.workers.Count}";
+
+				feature.workers.Add(new GadgetWorker(workerGadget.Build(stepIdentifier)));
+			});
+
+			return gadgetBuilder;
+		}
+
+		public static IGadgetBuilder UseWorker(
+			this IGadgetBuilder gadgetBuilder,
+			IGadgetBuilder workerGadget,
+			GadgetWorkerOptions workerGadgetOptions)
+		{
+			gadgetBuilder.Configure(configure =>
+			{
+				var feature = configure.Features.GetOrCreateFeature<GadgetWorkersFeature>();
+
+				string stepIdentifier = $"{configure.Identifier}-worker{feature.workers.Count}";
+
+				feature.workers.Add(new GadgetWorker(workerGadget.Build(stepIdentifier), workerGadgetOptions));
+			});
+
+			return gadgetBuilder;
+		}
+	}
+}
