702c116378bdf98a51cef2edab9b8cb3664b9076
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-09-21 02:55:05 +0100

    ✨ Added a way of running tasks on a gadget

---
+++ Gadgetry/Tasks/GadgetTasksFeature.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gadgetry.Tasks
+{
+	public class GadgetTasksFeature : IGadgetRunFeature
+	{
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		internal readonly List<GadgetTask> tasks = new();
+
+		public IReadOnlyList<GadgetTask> Tasks => tasks;
+
+		public GadgetTasksFeature()
+		{
+		}
+
+		async Task IGadgetRunFeature.RunAsync(GadgetRuntime gadgetRuntime, CancellationToken cancellationToken)
+		{
+			var awaitAll = new List<ValueTask>();
+
+			foreach (var task in Tasks)
+			{
+				var valueTask = task.TaskCallback(gadgetRuntime, cancellationToken);
+				awaitAll.Add(valueTask);
+			}
+
+			foreach (var awaitTarget in awaitAll)
+			{
+				await awaitTarget;
+			}
+		}
+	}
+}
