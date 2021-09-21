1885ba8a81b189675993531744cd48630b95231f
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-09-21 02:51:51 +0100

    ✨ Added a feature collection implementation
    This will provide gadgets with the ability to be arbitrarily extended

---
+++ Gadgetry/IReadOnlyFeatureCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Gadgetry
+{
+	public interface IReadOnlyFeatureCollection<TFeature> : IReadOnlyCollection<TFeature>
+		where TFeature : class, IFeature
+	{
+		IEnumerable<T> GetAllFeatures<T>() where T : class, TFeature;
+		T? GetFeature<T>() where T : class, TFeature;
+	}
+}
