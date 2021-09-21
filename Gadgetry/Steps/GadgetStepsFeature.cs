209a5fc9570ad64a4640af08dbc424bf6a467556
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-09-21 02:55:46 +0100

    ✨ Added a way of adding multiple sub-gadgets to run in sequence

---
+++ Gadgetry/Steps/GadgetStepsFeature.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gadgetry.Steps
+{
+	public class GadgetStepsFeature : IGadgetInitFeature, IGadgetRunFeature
+	{
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		internal readonly List<GadgetStep> steps = new();
+
+		public IReadOnlyList<GadgetStep> Steps => steps;
+
+		public GadgetStepsFeature()
+		{
+		}
+
+		void IGadgetInitFeature.Init(GadgetRuntime gadgetRuntime)
+		{
+			var runtimeFeature = gadgetRuntime.Features.GetOrCreateFeature<GadgetStepsRuntimeFeature>();
+
+			foreach (var step in Steps)
+			{
+				var stepRuntime = gadgetRuntime.ExtendWith(step.StepGadget);
+
+				runtimeFeature.steps.Add(stepRuntime);
+			}
+		}
+
+		async Task IGadgetRunFeature.RunAsync(GadgetRuntime gadgetRuntime, CancellationToken cancellationToken)
+		{
+			var runtimeFeature = gadgetRuntime.Features.GetOrCreateFeature<GadgetStepsRuntimeFeature>();
+
+			foreach (var stepRuntime in runtimeFeature.steps)
+			{
+				await stepRuntime.RunAsync(cancellationToken);
+			}
+		}
+	}
+}
