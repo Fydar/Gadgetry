3ed8655a47b584a56a7a0b82ec5c863444c9a205
fydar <fydar.dev@gmail.com>
2022-02-06 02:03:35 +0000

    🔥 Removing unused auto-initialised channel resources

--- Gadgetry/Resources/ResourceKey.cs
+++ Gadgetry/Resources/ResourceKey.cs
@@ -1,42 +1,7 @@
-﻿using System.Threading.Channels;
-using System.Threading.Tasks;
-
-namespace Gadgetry.Resources
+﻿namespace Gadgetry.Resources
 {
 	public class ResourceKey
 	{
-		public static AutoInitialisedResourceKey<Channel<TModel>> UnboundedChannel<TModel>()
-		{
-			return new AutoInitialisedResourceKey<Channel<TModel>>((gadgetRuntime, cancellationToken) =>
-			{
-				return ValueTask.FromResult(Channel.CreateUnbounded<TModel>());
-			});
-		}
-
-		public static AutoInitialisedResourceKey<Channel<TModel>> UnboundedChannel<TModel>(UnboundedChannelOptions options)
-		{
-			return new AutoInitialisedResourceKey<Channel<TModel>>((gadgetRuntime, cancellationToken) =>
-			{
-				return ValueTask.FromResult(Channel.CreateUnbounded<TModel>(options));
-			});
-		}
-
-		public static AutoInitialisedResourceKey<Channel<TModel>> BoundedChannel<TModel>(int capacity)
-		{
-			return new AutoInitialisedResourceKey<Channel<TModel>>((gadgetRuntime, cancellationToken) =>
-			{
-				return ValueTask.FromResult(Channel.CreateBounded<TModel>(capacity));
-			});
-		}
-
-		public static AutoInitialisedResourceKey<Channel<TModel>> BoundedChannel<TModel>(BoundedChannelOptions options)
-		{
-			return new AutoInitialisedResourceKey<Channel<TModel>>((gadgetRuntime, cancellationToken) =>
-			{
-				return ValueTask.FromResult(Channel.CreateBounded<TModel>(options));
-			});
-		}
-
 		public static AutoInitialisedResourceKey<TModel> AutoInitialised<TModel>()
 			where TModel : new()
 		{
